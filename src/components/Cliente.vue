840610acf4249f8b9ab8d37a361f814a84d449cd
Antonia Bustamante <anattolia@gmail.com>
2020-12-17 16:40:51 -0500

    Update Cliente.vue

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -252,5 +252,16 @@ export default {
     text-align: center;
     margin: 5%;
 }
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
 
 </style>
